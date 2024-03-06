b04eb9786a25e8aaecdde0c993f5046d952a7111
kainholy <ookuraseiya0506@gmail.com>
2024-03-06 16:19:10 +0900

    first commit

---
+++ src/components/sections/top/Mv.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '@/styles/sections/top/Mv.module.scss'
+
+function Mv() {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.bg}>
+      </div>
+
+      <div className={styles.slide}>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Image 
+              src="/top/mv/1.png"
+              alt="Your Image"
+              fill
+              className={styles.img}
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.titleArea}>
+        <h1 className={styles.title + ' mv-title --bk'}>駆けろ<span className={styles.titleBlue + ' --blue'}>青</span>春、轟け躍動</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Mv
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
+// 例えば、ページ間で共有されるナビゲーションバーやフッター、グローバルCSS、または状態管理ロジックなどをここに配置することができる
+
+import "@/styles/globals.scss";
+import "@/styles/normalize.scss";
+import "@/styles/mixin.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(d) {
+            var config = {
+              kitId: 'cro5gvm',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        `}} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }

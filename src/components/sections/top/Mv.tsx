b71d54312a90210d450e7c93b976eb3aceceed98
kainholy <ookuraseiya0506@gmail.com>
2024-04-18 11:05:19 +0900

    Mv追従

--- src/components/sections/top/Mv.tsx
+++ src/components/sections/top/Mv.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/sections/top/Mv.module.scss'
+import gsap from "gsap";
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 function Mv() {
+  const titleRef = useRef(null)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const title = titleRef.current
+    const container = containerRef.current
+    const titleHeight = title.clientHeight
+    const windowHeight = window.innerHeight
+    const initialTop = windowHeight - titleHeight - 40
+
+    gsap.set(title, {top: initialTop})
+
+    gsap.to(title, {
+      scrollTrigger: {
+        trigger: container,
+        start: 'top bottom',
+        end: 'bottom bottom',
+        scrub: 0,
+        onUpdate: (self) => {
+          const top = initialTop + self.scroll.v
+          if (self.scroll.v >= 253) {
+            gsap.set(title, {top: initialTop + 253})
+          } else {
+            gsap.set(title, {top: top})
+          }
+        }
+      }
+    })
+  })
+
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerRef}>
 
       <div className={styles.bg}>
       </div>
@@ -23,7 +57,7 @@ function Mv() {
         </ul>
       </div>
 
-      <div className={styles.titleArea}>
+      <div className={styles.titleArea} ref={titleRef}>
         <h1 className={styles.title + ' c-mv-title --bk'}>駆けろ<span className={styles.titleBlue + ' --blue'}>青</span>春、轟け躍動</h1>
       </div>
     </div>

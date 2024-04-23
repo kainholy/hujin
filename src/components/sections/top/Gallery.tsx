44ca81cf0b135a0bb225d09b2cd1dec736084713
kainholy <ookuraseiya0506@gmail.com>
2024-04-23 19:01:43 +0900

    message galleryセクション アニメーション実装

--- src/components/sections/top/Gallery.tsx
+++ src/components/sections/top/Gallery.tsx
@@ -1,27 +1,80 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/sections/top/Gallery.module.scss'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 function Gallery() {
+    const firstRef = useRef(null);
+    const secondRef = useRef(null);
+    const thirdRef = useRef(null);
+    const fourthRef = useRef(null);
+    const fifthRef = useRef(null);
+
+    useEffect(() => {
+        const first = firstRef.current;
+        const second = secondRef.current;
+        const third = thirdRef.current;
+        const fourth = fourthRef.current;
+        const fifth = fifthRef.current;
+        const contents = [first, second, third, fourth, fifth]
+
+        gsap.utils.toArray(contents).forEach((content, index) => {
+            if (index % 3 === 0 ) {
+                gsap.fromTo(content, { 'clip-path': 'inset(0 100% 0 0)', backgroundSize: "110%", y: 20 }, {
+                    y: 0,
+                    'clip-path': 'inset(0 0% 0 0)',
+                    backgroundSize: "100%",
+                    ease: 'power1.inOut',
+                    duration: .8,
+                    scrollTrigger: {
+                        trigger: content,
+                        start: 'top 70%',
+                    }
+                });
+            } else if (index % 2 === 0) {
+                gsap.fromTo(content, { 'clip-path': 'inset(0 0 0 100%)', backgroundSize: "110%", y: 20 }, {
+                    y: 0,
+                    'clip-path': 'inset(0 0 0 0%)',
+                    backgroundSize: "100%",
+                    ease: 'power1.inOut',
+                    duration: .8,
+                    scrollTrigger: {
+                        trigger: content,
+                        start: 'top 70%',
+                    }
+                });
+            } else {
+                gsap.fromTo(content, { 'clip-path': 'inset(0% 0 100% 0)', backgroundSize: "110%", y: 20 }, {
+                    y: 0,
+                    'clip-path': 'inset(0 0 0% 0)',
+                    backgroundSize: "100%",
+                    ease: 'power1.inOut',
+                    duration: .8,
+                    scrollTrigger: {
+                        trigger: content,
+                        start: 'top 70%',
+                    }
+                });
+            }
+        });
+    })
+
   return (
     <div className={styles.container}>
         <div className={styles.bgBigCircle}></div>
         <div className={styles.bgSmallCircle}></div>
         <div className={styles.content}>
-            <div className={styles.firstImgContainer +' '+ styles.imgContainer}>
-                <Image className={styles.img} src='/top/gallery/1.webp' alt='hero' width={2000} height={2000} ></Image>
+            <div className={styles.firstImgContainer +' '+ styles.imgContainer} ref={firstRef}>
             </div>
-            <div className={styles.secondImgContainer +' '+ styles.imgContainer}>
-                <Image className={styles.img} src='/top/gallery/2.webp' alt='hero' width={2000} height={2000} ></Image>
+            <div className={styles.secondImgContainer +' '+ styles.imgContainer} ref={secondRef}>
             </div>
-            <div className={styles.thirdImgContainer +' '+ styles.imgContainer}>
-                <Image className={styles.img} src='/top/gallery/3.webp' alt='hero' width={2000} height={2000} ></Image>
+            <div className={styles.thirdImgContainer +' '+ styles.imgContainer} ref={thirdRef}>
             </div>
-            <div className={styles.fourthImgContainer +' '+ styles.imgContainer}>
-                <Image className={styles.img} src='/top/gallery/4.webp' alt='hero' width={2000} height={2000} ></Image>
+            <div className={styles.fourthImgContainer +' '+ styles.imgContainer} ref={fourthRef}>
             </div>
-            <div className={styles.fifthImgContainer +' '+ styles.imgContainer}>
-                <Image className={styles.img} src='/top/gallery/5.webp' alt='hero' width={2000} height={2000} ></Image>
+            <div className={styles.fifthImgContainer +' '+ styles.imgContainer} ref={fifthRef}>
             </div>
         </div>
     </div>
--- src/components/sections/top/Message.tsx
+++ src/components/sections/top/Message.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/sections/top/Message.module.scss'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 function Message() {
+  const line1Ref = useRef(null);
+  const line2Ref = useRef(null);
+  const line3Ref = useRef(null);
+  const line4Ref = useRef(null);
+  const line5Ref = useRef(null);
+  const line6Ref = useRef(null);
+  const line7Ref = useRef(null);
+  const line8Ref = useRef(null);
+  const line9Ref = useRef(null);
+  const line10Ref = useRef(null);
+  const line11Ref = useRef(null);
+  const line12Ref = useRef(null);
+
+  useEffect(() => {
+    const line1 = line1Ref.current;
+    const line2 = line2Ref.current;
+    const line3 = line3Ref.current;
+    const line4 = line4Ref.current;
+    const line5 = line5Ref.current;
+    const line6 = line6Ref.current;
+    const line7 = line7Ref.current;
+    const line8 = line8Ref.current;
+    const line9 = line9Ref.current;
+    const line10 = line10Ref.current;
+    const line11 = line11Ref.current;
+    const line12 = line12Ref.current;
+    const line1Height = line1.clientHeight + 50;
+    const lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12]
+
+    gsap.utils.toArray(lines).forEach((line, index) => {
+      gsap.fromTo(line, { backgroundPositionY: '100%', y: 10 }, {
+        backgroundPositionY: '-25%',
+        y: 0,
+        scrollTrigger: {
+          trigger: line,
+          start: 'top 95%',
+          end: '+='+line1Height,
+          scrub: true
+        }
+      });
+    });
+
+
+
+  })
   return (
     <section className={styles.container} id='message'>
       <div className={styles.leftCircle}>
@@ -12,22 +60,22 @@ function Message() {
       <div className={styles.contentArea}>
         <div className={styles.textArea}>
             <p className={styles.text + ' c-message-text'}>
-                踊って、笑って、語り合う。<br />
-                そんな温かな時間を通じて、<br />
-                この世界に元気と笑顔を届けたい。<br /><br />
+                <span className={styles.line} ref={line1Ref}>踊って、笑って、語り合う。</span><br />
+                <span className={styles.line} ref={line2Ref}>そんな温かな時間を通じて、</span><br />
+                <span className={styles.line} ref={line3Ref}>この世界に元気と笑顔を届けたい。</span><br /><br />
 
-                だからこそ、<br />
-                私たちはいつも失敗を恐れず、<br />
-                笑顔を忘れずにいます。<br /><br />
+                <span className={styles.line} ref={line4Ref}>だからこそ、</span><br />
+                <span className={styles.line} ref={line5Ref}>私たちはいつも失敗を恐れず、</span><br />
+                <span className={styles.line} ref={line6Ref}>笑顔を忘れずにいます。</span><br /><br />
 
-                『誰かを楽しませたい』<br />
-                それが風神の信念です。<br /><br />
+                <span className={styles.line} ref={line7Ref}>『誰かを楽しませたい』</span><br />
+                <span className={styles.line} ref={line8Ref}>それが風神の信念です。</span><br /><br />
 
-                それが逆風にも負けず、<br className='sp'/>自由に笑顔を広げる、<br />
-                貴重な存在になるはずだから。<br /><br />
+                <span className={styles.line} ref={line9Ref}>それが逆風にも負けず、<br className='sp'/>自由に笑顔を広げる、</span><br />
+                <span className={styles.line} ref={line10Ref}>貴重な存在になるはずだから。</span><br /><br />
 
-                明るい未来を作るために、<br />
-                今日も小さな世界を明るく照らします。
+                <span className={styles.line} ref={line11Ref}>明るい未来を作るために、</span><br />
+                <span className={styles.line} ref={line12Ref}>今日も小さな世界を明るく照らします。</span>
             </p>
         </div>
         <div className={styles.titleArea}>

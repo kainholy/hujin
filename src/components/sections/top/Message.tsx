f919c26a6a3fe9360a20ff03192a871d7b4d9a1e
kainholy <ookuraseiya0506@gmail.com>
2024-04-24 11:47:07 +0900

    修正

--- src/components/sections/top/Message.tsx
+++ src/components/sections/top/Message.tsx
@@ -14,10 +14,13 @@ function Message() {
   const line6Ref = useRef(null);
   const line7Ref = useRef(null);
   const line8Ref = useRef(null);
-  const line9Ref = useRef(null);
+  const line91Ref = useRef(null);
+  const line92Ref = useRef(null);
+  const line93Ref = useRef(null);
   const line10Ref = useRef(null);
   const line11Ref = useRef(null);
   const line12Ref = useRef(null);
+  const imgRef = useRef(null);
 
   useEffect(() => {
     const line1 = line1Ref.current;
@@ -28,16 +31,20 @@ function Message() {
     const line6 = line6Ref.current;
     const line7 = line7Ref.current;
     const line8 = line8Ref.current;
-    const line9 = line9Ref.current;
+    const line91 = line91Ref.current;
+    const line92 = line92Ref.current;
+    const line93 = line93Ref.current;
     const line10 = line10Ref.current;
     const line11 = line11Ref.current;
     const line12 = line12Ref.current;
+    const img = imgRef.current;
+
     const line1Height = line1.clientHeight + 50;
-    const lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12]
+    const lines = [line1, line2, line3, line4, line5, line6, line7, line8, line91, line92, line93, line10, line11, line12]
 
     gsap.utils.toArray(lines).forEach((line, index) => {
       gsap.fromTo(line, { backgroundPositionY: '100%', y: 10 }, {
-        backgroundPositionY: '-25%',
+        backgroundPositionY: '-23%',
         y: 0,
         scrollTrigger: {
           trigger: line,
@@ -46,6 +53,15 @@ function Message() {
           scrub: true
         }
       });
+    });
+    gsap.fromTo(img, { backgroundSize: '110%' }, {
+      backgroundSize: '100%',
+      scrollTrigger: {
+        trigger: img,
+        start: 'top 90%',
+        end: 'bottom 40%',
+        scrub: true
+      }
     });
 
 
@@ -71,7 +87,9 @@ function Message() {
                 <span className={styles.line} ref={line7Ref}>『誰かを楽しませたい』</span><br />
                 <span className={styles.line} ref={line8Ref}>それが風神の信念です。</span><br /><br />
 
-                <span className={styles.line} ref={line9Ref}>それが逆風にも負けず、<br className='sp'/>自由に笑顔を広げる、</span><br />
+                <span className={styles.line + ' pc'} ref={line91Ref}>それが逆風にも負けず、自由に笑顔を広げる、</span><br className='pc' />
+                <span className={styles.line + ' sp'} ref={line92Ref}>それが逆風にも負けず、</span><br className='sp'/>
+                <span className={styles.line + ' sp'} ref={line93Ref}>自由に笑顔を広げる、</span><br className='sp' />
                 <span className={styles.line} ref={line10Ref}>貴重な存在になるはずだから。</span><br /><br />
 
                 <span className={styles.line} ref={line11Ref}>明るい未来を作るために、</span><br />
@@ -85,8 +103,7 @@ function Message() {
             </h3>
         </div>
       </div>
-      <div className={styles.imgArea}>
-        <Image className={styles.img} src="/top/message/1.png" alt="message" width={500} height={500} />
+      <div className={styles.imgArea} ref={imgRef}>
       </div>
     </section>
   )
